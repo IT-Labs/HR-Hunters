a4f9df1a881f578b5287070e9f503a595f856f38
Tijana Tasevska Stokic <tijana.tasevska@gmail.com>
2018-11-18 18:25:14 +0100

    added functionality on clickable links in menu

--- Frontend/src/app/admin-dashboard/admin-dashboard.component.ts
+++ Frontend/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -6,34 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.scss']
 })
 export class AdminDashboardComponent implements OnInit {
-  // selectedUserTab = 1;
-  // tabs = [
-  //   {
-  //     name: 'Dashboard',
-  //     key: 1,
-  //     active: true
-  //   },
-  //    {
-  //    name: 'Jobs',
-  //    key: 2,
-  //    active: false
-  //  },
-  //  {
-  //    name: 'Aplications',
-  //    key: 3,
-  //    active: false
-  //  },
-  //  {
-  //    name: 'Clients',
-  //    key: 4,
-  //    active: false
-  //  },
-  //  {
-  //   name: 'Aplicants',
-  //   key: 5,
-  //   active: false
-  // },
-  // ];
+  selectedUserTab = 1;
+  tabs = [
+    {
+      name: 'Dashboard',
+      key: 1,
+      active: true
+    },
+     {
+     name: 'Jobs',
+     key: 2,
+     active: false
+   },
+   {
+     name: 'Aplications',
+     key: 3,
+     active: false
+   },
+   {
+     name: 'Clients',
+     key: 4,
+     active: false
+   },
+   {
+    name: 'Aplicants',
+    key: 5,
+    active: false
+  },
+  ];
  
   constructor() { }
 
