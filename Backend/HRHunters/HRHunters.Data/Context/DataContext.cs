c3015d2bb386f03fd726c5efce95faeb42665ef1
Vlatko Zmejkoski <vlatkozmejkoski@gmail.com>
2018-11-19 15:57:36 +0100

    Database connection on test

--- Backend/HRHunters/HRHunters.Data/Context/DataContext.cs
+++ Backend/HRHunters/HRHunters.Data/Context/DataContext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using HRHunters.Data.Context;
 using HRHunters.Common.Entities;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace HRHunters.Data.Context
 {
@@ -16,5 +17,17 @@ namespace HRHunters.Data.Context
         public DbSet<Client> Clients { get; set; }
         public DbSet<JobPosting> JobPostings { get; set; }
         public DbSet<User> Users{ get; set; }
+
+
+        public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+        {
+            public DataContext CreateDbContext(string[] args)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+                optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=test;User Id=postgres;Password=;");
+
+                return new DataContext(optionsBuilder.Options);
+            }
+        }
     }
 }
