e610c5d99445542c527d61a85b85e7de2a343dd8
Ivo Kostovski <ivo.kostovski@gmail.com>
2018-11-21 16:41:58 +0100

    new jobs form added

--- Frontend/src/app/authentication/clients/register/register.component.ts
+++ Frontend/src/app/authentication/clients/register/register.component.ts
@@ -1,28 +1,56 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"]
 })
 export class ClientRegisterComponent {
-
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   clientRegisterForm = this.fb.group({
-    clientName: ['', Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(50), Validators.pattern('[a-zA-Z ]*')])],
-    clientEmail: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(30), Validators.email])],
-    clientPassword: ['', Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(20)])],
-    clientConfirmPassword: ['', Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(20)])]
-  })
-
+    clientName: [
+      "",
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(50),
+        Validators.pattern("[a-zA-Z ]*")
+      ])
+    ],
+    clientEmail: [
+      "",
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(30),
+        Validators.email
+      ])
+    ],
+    clientPassword: [
+      "",
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20)
+      ])
+    ],
+    clientConfirmPassword: [
+      "",
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20)
+      ])
+    ]
+  });
 
   onClientRegister() {
     console.log(this.clientRegisterForm.value);
 
     if (this.clientRegisterForm.valid) {
-      this.clientRegisterForm.reset()
+      this.clientRegisterForm.reset();
     }
   }
 }
