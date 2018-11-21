6c34fb92d1742e73bd5e998ee01057adc9f363cb
Vlatko Zmejkoski <vlatkozmejkoski@gmail.com>
2018-11-21 11:25:24 +0100

    Role/UserRole

---
+++ Backend/HRHunters/HRHunters.Common/Entities/Role.cs
@@ -0,0 +1,32 @@
+﻿using HRHunters.Common.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace HRHunters.Common.Entities
+{
+    public class Role : IdentityRole<int>, IEntity<int>
+    {
+        public ICollection<UserRole> UserRoles { get; set; }
+        private DateTime? createdDate;
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedDate
+        {
+            get { return createdDate ?? DateTime.UtcNow; }
+            set => createdDate = value;
+        }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? ModifiedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+
+        object IEntity.Id
+        {
+            get { return Id; }
+        }
+    }
+}
