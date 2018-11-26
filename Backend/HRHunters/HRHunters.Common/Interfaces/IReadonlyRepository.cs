3921e392ca149c2d8dbc5e049aadb45a3fe76e74
Vlatko Zmejkoski <vlatkozmejkoski@gmail.com>
2018-11-26 16:34:52 +0100

    More ignored files..

---
+++ Backend/HRHunters/HRHunters.Common/Interfaces/IReadonlyRepository.cs
@@ -0,0 +1,38 @@
+﻿using HRHunters.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace HRHunters.Data
+{
+    public interface IReadonlyRepository
+    {
+        IEnumerable<TEntity> GetAll<TEntity>(Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+        string includeProperties = null,
+        int? skip = null,
+        int? take = null)
+        where TEntity : class, IEntity;
+
+        IEnumerable<TEntity> Get<TEntity>(Expression<Func<TEntity, bool>> filter = null,Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+        string includeProperties = null,
+        int? skip = null,
+        int? take = null)
+        where TEntity : class, IEntity;
+
+        TEntity GetOne<TEntity>(
+        Expression<Func<TEntity, bool>> filter = null,
+        string includeProperties = null)
+        where TEntity : class, IEntity;
+
+        TEntity GetById<TEntity>(object id)
+        where TEntity : class, IEntity;
+
+        int GetCount<TEntity>(Expression<Func<TEntity, bool>> filter = null)
+        where TEntity : class, IEntity;
+
+        bool GetExists<TEntity>(Expression<Func<TEntity, bool>> filter = null)
+        where TEntity : class, IEntity;
+    }
+}
---
+++ Backend/HRHunters/HRHunters.Common/Interfaces/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using HRHunters.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRHunters.Data
+{
+    public interface IRepository:IReadonlyRepository
+    {
+        void Create<TEntity>(TEntity entity, string createdBy = null)
+        where TEntity : class, IEntity;
+
+        void Update<TEntity>(TEntity entity, string modifiedBy = null)
+            where TEntity : class, IEntity;
+
+        void Delete<TEntity>(object id)
+            where TEntity : class, IEntity;
+
+        void Delete<TEntity>(TEntity entity)
+            where TEntity : class, IEntity;
+
+        void Save();
+    }
+}
