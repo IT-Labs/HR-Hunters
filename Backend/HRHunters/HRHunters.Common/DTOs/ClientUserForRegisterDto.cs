49245af66ce0aee81dca225816c2b71dea7dd9b1
Vlatko Zmejkoski <vlatkozmejkoski@gmail.com>
2018-11-26 16:29:14 +0100

    unpushed dto class added

---
+++ Backend/HRHunters/HRHunters.Common/DTOs/ClientUserForRegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRHunters.Common.DTOs
+{
+    public class ClientUserForRegisterDto
+    {
+        public string CompanyName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
