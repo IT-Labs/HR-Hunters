31023f988f736e8ef1ad21ca786943592d5cc8dd
Vlatko Zmejkoski <vlatkozmejkoski@gmail.com>
2018-11-29 15:29:32 +0100

    Ignored File

---
+++ Backend/HRHunters/HRHunters.Common/Responses/UserToReturnModel.cs
@@ -0,0 +1,20 @@
+﻿using HRHunters.Common.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRHunters.Common.Responses
+{
+    public class UserToReturnModel
+    {
+        public bool Succedeed { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CompanyName { get; set; }
+        public string Token { get; set; }
+        public string Email { get; set; }
+        public int Id { get; set; }
+        public int Role { get; set; }
+    }
+}
