b03f0ba0e08787cc88c86e951b5f9f36af24a9d2
Tome Tasevski <tasevski.tome@gmail.com>
2018-12-03 13:47:01 +0100

    Validation fixes

--- Backend/HRHunters/HRHunters.Common/Responses/UserLoginReturnModel.cs
+++ Backend/HRHunters/HRHunters.Common/Responses/UserLoginReturnModel.cs
@@ -10,7 +10,6 @@ namespace HRHunters.Common.Responses
         public bool Succeeded { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string CompanyName { get; set; }
         public string Token { get; set; }
         public string Email { get; set; }
         public int Id { get; set; }
