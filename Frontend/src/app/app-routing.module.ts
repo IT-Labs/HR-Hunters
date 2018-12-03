03ee9e9ce45dcd3d9fb20b09f1f2e0eba3919b15
Ivo Kostovski <ivo.kostovski@gmail.com>
2018-12-03 13:18:01 +0100

    reconfigure of component structure and routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from "@angular/router";
 import { ClientRegisterComponent } from "./authentication/clients/register/register.component";
 import { ApplicantRegisterComponent } from "./authentication/applicants/register/register.component";
 import { AddJobPostingComponent } from "./homepage/client/add-job-posting/add-job-posting.component";
-import { ApplicantsComponent } from "./authentication/applicants/applicants.component";
 import { ClientComponent } from "./homepage/client/client.component";
 import { ClientJobPostingsComponent } from "./homepage/client/client-job-postings/client-job-postings.component";
 import { JobPostingDetailsComponent } from "./homepage/applicant/job-posting-details/job-posting-details.component";
@@ -14,27 +13,32 @@ import { ClientProfileComponent } from "./homepage/client/client-profile/client-
 import { ApplicantProfileComponent } from "./homepage/applicant/applicant-profile/applicant-profile.component";
 import { WelcomeComponent } from "./authentication/welcome.component";
 import { AdminDashboardComponent } from "./admin-dashboard/admin-dashboard.component";
-import { LoginComponent } from './authentication/login/login.component';
+import { LoginComponent } from "./authentication/login/login.component";
 import { ADApplicantsComponent } from "./admin-dashboard/applicants/applicants.component";
 import { ADApplicationsComponent } from "./admin-dashboard/applications/applications.component";
 import { ADClientsComponent } from "./admin-dashboard/clients/clients.component";
 import { ADJobPostingsComponent } from "./admin-dashboard/job-postings/job-postings.component";
 import { ADNewJobPostingComponent } from "./admin-dashboard/job-postings/new-job-posting/new-job-posting.component";
 import { AuthGuard } from "./authentication/auth.guard";
+import { ApplicantComponent } from "./homepage/applicant/applicant.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: 'welcome', pathMatch: 'full' },
+  { path: "", redirectTo: "welcome", pathMatch: "full" },
   { path: "welcome", component: WelcomeComponent },
   { path: "login", component: LoginComponent },
   { path: "client-register", component: ClientRegisterComponent },
   { path: "applicant-register", component: ApplicantRegisterComponent },
-  { path: "admin-dashboard", component: AdminDashboardComponent, children: [
-    { path: "applicants", component: ADApplicantsComponent },
-    { path: "applications", component: ADApplicationsComponent },
-    { path: "clients", component: ADClientsComponent },
-    { path: "job-postings", component: ADJobPostingsComponent },
-    { path: "new-job-posting", component: ADNewJobPostingComponent }
-  ]},
+  {
+    path: "admin-dashboard",
+    component: AdminDashboardComponent,
+    children: [
+      { path: "applicants", component: ADApplicantsComponent },
+      { path: "applications", component: ADApplicationsComponent },
+      { path: "clients", component: ADClientsComponent },
+      { path: "job-postings", component: ADJobPostingsComponent },
+      { path: "new-job-posting", component: ADNewJobPostingComponent }
+    ]
+  },
   {
     path: "client",
     component: ClientComponent,
@@ -47,7 +51,7 @@ const routes: Routes = [
   },
   {
     path: "applicant",
-    component: ApplicantsComponent,
+    component: ApplicantComponent,
     children: [
       { path: "profile", component: ApplicantProfileComponent },
       { path: "job-postings", component: AppicantJobPostingsComponent },
