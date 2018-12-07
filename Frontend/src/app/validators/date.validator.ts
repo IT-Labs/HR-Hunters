f54f37ceb6f08c13dd76adb0fc1d8ebfa0554957
Ivo Kostovski <ivo.kostovski@gmail.com>
2018-12-07 11:46:25 +0100

    new date feature implemented

--- Frontend/src/app/validators/date.validator.ts
+++ Frontend/src/app/validators/date.validator.ts
@@ -4,9 +4,36 @@ export function DateValidator(control: AbstractControl) {
     if (control && (control.value !== null || control.value !== undefined)) {
         const durationTo = new Date(control.value);
         const dateControl = control.root.get('durationFrom');
+
+        const date = new Date();
+        const dd = date.getDay()
+        const mm = date.getMonth()+1
+        const yyyy = date.getFullYear()
+        let newDD;
+        let newMM;
+        if (dd < 10) {
+            newDD = `0${dd}`
+        } else {
+            newDD = dd
+        }
+        if (mm < 10) {
+            newMM = `0${mm}`
+        } else {
+            newMM = mm
+        }
+        
+        const today = `${yyyy}-${newMM}-${newDD}`
         
         if (dateControl) {
             const dateFromValue = new Date(dateControl.value);
+
+            const todayDate = new Date(today);
+
+            if (dateFromValue < todayDate) {
+                return {
+                    isError: true
+                }
+            }
             if (dateFromValue >= durationTo) {
                 return {
                     isError: true
