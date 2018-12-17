84bfa51438c897ef05f769377f45fbc48a71c30f
Ivo Kostovski <ivo.kostovski@gmail.com>
2018-12-17 15:29:28 +0100

    fixed logged in user recongnize

--- Frontend/src/app/authentication/login/login.component.ts
+++ Frontend/src/app/authentication/login/login.component.ts
@@ -13,6 +13,7 @@ export class LoginComponent implements OnInit {
   role = 3;
   loading = false;
   authError;
+  loggedInUser = null;
 
   private authErrorStatusSub: Subscription;
   private roleStatusSub: Subscription;
@@ -25,6 +26,21 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     this.loading = true;
+    this.loggedInUser = this.authService.getUser();
+
+    if (this.loggedInUser !== null) {
+      if (this.loggedInUser.role === 1) {
+        this.router.navigate(['/applicant/job-postings'])
+        return
+      } else if (this.loggedInUser.role === 2) {
+        this.router.navigate(['/client/job-postings'])
+        return
+      } else if (this.loggedInUser.role === 3) {
+        this.router.navigate(['/admin-dashboard/job-postings'])
+        return
+      }
+    }
+
     this.role = this.authService.getRole();
     
     this.authErrorStatusSub = this.authService
