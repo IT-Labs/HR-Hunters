0cbcb3be7fe4528588b86d55593f755437fd5312
Ivo Kostovski <ivo.kostovski@gmail.com>
2018-12-19 15:53:24 +0100

    bug fix

--- Frontend/src/app/homepage/header/navigation/navigation.component.ts
+++ Frontend/src/app/homepage/header/navigation/navigation.component.ts
@@ -1,33 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  selector: "app-navigation",
+  templateUrl: "./navigation.component.html",
+  styleUrls: ["./navigation.component.scss"]
 })
 export class NavigationComponent implements OnInit {
-
   activeApplicant = true;
-  name = 'Josh'
-  test = '';
+  name = "Josh";
+  test = "";
   loggedInUser;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {  
-   this.loggedInUser = this.authService.getUser();
+  ngOnInit() {
+    this.loggedInUser = this.authService.getUser();
 
-   this.name = `${this.loggedInUser.firstName}`;
+    this.name = `${this.loggedInUser.firstName}`;
 
-   if (this.loggedInUser.role === 1) {
-     this.activeApplicant = true;
-   } else if (this.loggedInUser.role === 2) {
-     this.activeApplicant = false;
-   }
+    if (this.loggedInUser.role === 1) {
+      this.activeApplicant = true;
+    } else if (this.loggedInUser.role === 2) {
+      this.activeApplicant = false;
+    }
+  }
+
+  // Closing the dropdown on click on link
+  onDropdownClick() {
+    const dropdown = document.getElementById("homepage-navigation");
+
+    if (dropdown.classList.contains("show")) {
+      dropdown.classList.remove("show");
+    }
   }
 
   onLogout() {
-    this.authService.logout()
+    this.authService.logout();
   }
 }
