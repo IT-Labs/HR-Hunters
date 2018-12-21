b162af0f188f5672326f1ef7a848f23a31b5e6ac
Vlatko Zmejkoski <vlatkozmejkoski@gmail.com>
2018-12-21 16:16:19 +0100

    Email sender implemented, needs polishing with template

---
+++ Backend/HRHunters/HRHunters.Common/Interfaces/IEmailSenderManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRHunters.Common.Interfaces
+{
+    public interface IEmailSenderManager
+    {
+        Task SendEmail(string from, List<string> MailingList);
+    }
+}
