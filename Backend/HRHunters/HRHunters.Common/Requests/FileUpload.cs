a531b587d7042460b4b8c1f4ba76d571f490be70
Vlatko Zmejkoski <vlatkozmejkoski@gmail.com>
2018-12-25 19:13:07 +0100

    Code remarks

---
+++ Backend/HRHunters/HRHunters.Common/Requests/FileUpload.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRHunters.Common.Requests
+{
+    public class FileUpload
+    {
+        public IFormFile FormFile { get; set; }
+        public int Id { get; set; }
+    }
+}
